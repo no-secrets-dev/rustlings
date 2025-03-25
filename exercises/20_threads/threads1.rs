1b6bafacf5f03073a59cf5c6dd3c61ce0c224f56
no-secrets-dev <nosecrets1123@gmail.com>
2025-03-25 04:14:10 -0400

    add threads

--- exercises/20_threads/threads1.rs
+++ exercises/20_threads/threads1.rs
@@ -27,6 +27,9 @@ fn main() {
     let mut results: Vec<u128> = vec![];
     for handle in handles {
         // TODO: a struct is returned from thread::spawn, can you use it?
+        if let Ok(elapsed) = handle.join() {
+            results.push(elapsed);
+        }
     }
 
     if results.len() != 10 {
