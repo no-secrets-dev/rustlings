89f6da2eef3a73d860642c68e15b0a57e42e56ed
no-secrets-dev <nosecrets1123@gmail.com>
2025-03-29 21:14:21 -0400

    add test comment

--- exercises/16_lifetimes/lifetimes3.rs
+++ exercises/16_lifetimes/lifetimes3.rs
@@ -5,6 +5,8 @@
 // Execute `rustlings hint lifetimes3` or use the `hint` watch subcommand for a
 // hint.
 // THIS IS A TEST COMMENT
+// THIS IS ANOTHER TEST COMMENT
+
 
 struct Book<'a> {
     author: &'a str,
